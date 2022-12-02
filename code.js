feb540f78aa4d4f3c37592fb56ee8eb67bb34948
Aditya <115468905+Aditya030503@users.noreply.github.com>
2022-12-02 12:17:22 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,608 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head><style>
+    .continer{
+display: grid;
+grid-template-columns: repeat(18,1fr);
+grid-template-rows: repeat(7,80px);
+gap: 10px;
+width: 90%;
+margin: auto;
+margin-top: 5%;
+}
+.continer>div{
+    background-color: black;
+    text-align: center;
+    border-radius: 5px;
+}
+.continer>div:nth-child(1){
+    background-color: green;
+}
+.continer>div:nth-child(2){
+    background-color: white;
+}
+.continer>div:nth-child(3){
+    background-color: white;
+}
+.continer>div:nth-child(4){
+    background-color: white;
+}.continer>div:nth-child(5){
+    background-color: white;
+}.continer>div:nth-child(6){
+    background-color: white;
+}.continer>div:nth-child(7){
+    background-color: white;
+}.continer>div:nth-child(8){
+    background-color: white;
+}.continer>div:nth-child(9){
+    background-color: white;
+}.continer>div:nth-child(10){
+    background-color: white;
+}.continer>div:nth-child(11){
+    background-color: white;
+}.continer>div:nth-child(12){
+    background-color: white;
+}.continer>div:nth-child(13){
+    background-color: white;
+}.continer>div:nth-child(14){
+    background-color: white;
+}.continer>div:nth-child(15){
+    background-color: white;
+}.continer>div:nth-child(16){
+    background-color: white;
+}.continer>div:nth-child(17){
+    background-color: white;
+}.continer>div:nth-child(18){
+    background-color: rgb(223, 223, 28);
+}
+.continer>div:nth-child(19){
+    background-color: rgb(211, 17, 17);
+}
+.continer>div:nth-child(20){
+    background-color: gray;
+}.continer>div:nth-child(21){
+    background-color: white;
+}.continer>div:nth-child(22){
+    background-color: white;
+}.continer>div:nth-child(23){
+    background-color: white;
+}.continer>div:nth-child(24){
+    background-color: white;
+}.continer>div:nth-child(25){
+    background-color: white;
+}.continer>div:nth-child(26){
+    background-color: white;
+}.continer>div:nth-child(27){
+    background-color: white;
+}.continer>div:nth-child(28){
+    background-color: white;
+}.continer>div:nth-child(29){
+    background-color: white;
+}.continer>div:nth-child(30){
+    background-color: white;
+}
+.continer>div:nth-child(31){
+    background-color: rgb(223, 12, 191);
+}
+.continer>div:nth-child(32){
+    background-color: green;
+}
+.continer>div:nth-child(33){
+    background-color: green;
+}
+.continer>div:nth-child(34){
+    background-color: green;
+}
+.continer>div:nth-child(35){
+    background-color: rgb(212, 100, 14);
+}
+.continer>div:nth-child(36){
+    background-color: rgb(223, 223, 28);
+}
+.continer>div:nth-child(37){
+    background-color: rgb(211, 17, 17);
+}
+.continer>div:nth-child(38){
+    background-color: gray;
+}.continer>div:nth-child(39){
+    background-color: white;
+}.continer>div:nth-child(40){
+    background-color: white;
+}.continer>div:nth-child(41){
+    background-color: white;
+}.continer>div:nth-child(42){
+    background-color: white;
+}.continer>div:nth-child(43){
+    background-color: white;
+}.continer>div:nth-child(44){
+    background-color: white;
+}.continer>div:nth-child(45){
+    background-color: white;
+}.continer>div:nth-child(46){
+    background-color: white;
+}.continer>div:nth-child(47){
+    background-color: white;
+}.continer>div:nth-child(48){
+    background-color: white;
+}
+.continer>div:nth-child(49){
+    background-color: blueviolet;
+}
+.continer>div:nth-child(50){
+    background-color: rgb(223, 12, 191);
+}
+.continer>div:nth-child(51){
+    background-color: green;
+}
+.continer>div:nth-child(52){
+    background-color: green;
+}
+.continer>div:nth-child(53){
+    background-color: rgb(212, 100, 14);
+}
+.continer>div:nth-child(54){
+    background-color: rgb(223, 223, 28);
+}.continer>div:nth-child(55){
+    background-color: rgb(211, 17, 17);
+}
+.continer>div:nth-child(56){
+    background-color: gray;
+}
+.continer>div:nth-child(57){
+    background-color: orange;
+}.continer>div:nth-child(58){
+    background-color: orange;
+}.continer>div:nth-child(59){
+    background-color: orange;
+}.continer>div:nth-child(60){
+    background-color: orange;
+}.continer>div:nth-child(61){
+    background-color: orange;
+}.continer>div:nth-child(62){
+    background-color: orange;
+}.continer>div:nth-child(63){
+    background-color: orange;
+}.continer>div:nth-child(64){
+    background-color: orange;
+}.continer>div:nth-child(65){
+    background-color: orange;
+}.continer>div:nth-child(66){
+    background-color: orange;
+}
+.continer>div:nth-child(67){
+    background-color: blueviolet;
+}
+.continer>div:nth-child(68){
+    background-color: rgb(223, 12, 191);
+}.continer>div:nth-child(69){
+    background-color: rgb(223, 12, 191);
+}.continer>div:nth-child(70){
+    background-color: green;
+}.continer>div:nth-child(71){
+    background-color: rgb(212, 100, 14);
+}
+.continer>div:nth-child(72){
+    background-color: rgb(223, 223, 28);
+}
+.continer>div:nth-child(73){
+    background-color: rgb(211, 17, 17);
+}
+.continer>div:nth-child(74){
+    background-color: gray;
+}
+.continer>div:nth-child(75){
+    background-color: orange;
+}.continer>div:nth-child(76){
+    background-color: orange;
+}.continer>div:nth-child(77){
+    background-color: orange;
+}.continer>div:nth-child(78){
+    background-color: orange;
+}.continer>div:nth-child(79){
+    background-color: orange;
+}.continer>div:nth-child(80){
+    background-color: orange;
+}.continer>div:nth-child(81){
+    background-color: orange;
+}.continer>div:nth-child(82){
+    background-color: orange;
+}.continer>div:nth-child(83){
+    background-color: orange;
+}.continer>div:nth-child(84){
+    background-color: orange;
+}
+.continer>div:nth-child(85){
+    background-color: blueviolet;
+}.continer>div:nth-child(86){
+    background-color: blueviolet;
+}
+.continer>div:nth-child(87){
+    background-color:rgb(223, 12, 191) ;
+}.continer>div:nth-child(88){
+    background-color: rgb(223, 12, 191);
+}.continer>div:nth-child(89){
+    background-color: rgb(212, 100, 14);
+}
+.continer>div:nth-child(90){
+    background-color: rgb(223, 223, 28);
+}
+.continer>div:nth-child(91){
+    background-color: rgb(211, 17, 17);
+}
+.continer>div:nth-child(92){
+    background-color: gray;
+}
+.continer>div:nth-child(93){
+    background-color: white;
+}.continer>div:nth-child(94){
+    background-color: orange;
+}.continer>div:nth-child(95){
+    background-color: orange;
+}.continer>div:nth-child(96){
+    background-color: orange;
+}.continer>div:nth-child(97){
+    background-color: orange;
+}.continer>div:nth-child(98){
+    background-color: orange;
+}.continer>div:nth-child(99){
+    background-color: orange;
+}.continer>div:nth-child(100){
+    background-color: orange;
+}.continer>div:nth-child(101){
+    background-color: orange;
+}.continer>div:nth-child(102){
+    background-color: orange;
+}
+.continer>div:nth-child(103){
+    background-color: blueviolet;
+}.continer>div:nth-child(104){
+    background-color: blueviolet;
+}
+.continer>div:nth-child(105){
+    background-color: blueviolet;
+}.continer>div:nth-child(106){
+    background-color: rgb(223, 12, 191);
+}.continer>div:nth-child(107){
+    background-color: rgb(212, 100, 14);
+}
+.continer>div:nth-child(108){
+    background-color: rgb(223, 223, 28);
+}
+.continer>div:nth-child(109){
+    background-color: rgb(211, 17, 17);
+}
+.continer>div:nth-child(110){
+    background-color: gray;
+}
+.continer>div:nth-child(111){
+    background-color: white;
+}.continer>div:nth-child(112){
+    background-color: orange;
+}.continer>div:nth-child(113){
+    background-color: orange;
+}.continer>div:nth-child(114){
+    background-color: orange;
+}.continer>div:nth-child(115){
+    background-color: orange;
+}.continer>div:nth-child(116){
+    background-color: orange;
+}.continer>div:nth-child(117){
+    background-color: orange;
+}.continer>div:nth-child(118){
+    background-color: brown;
+}.continer>div:nth-child(119){
+    background-color: brown;
+}.continer>div:nth-child(120){
+    background-color: orange;
+}
+.continer>div:nth-child(121){
+    background-color: brown;
+}.continer>div:nth-child(122){
+    background-color: brown;
+}
+.continer>div:nth-child(123){
+    background-color: brown;
+}.continer>div:nth-child(124){
+    background-color: brown;
+}.continer>div:nth-child(125){
+    background-color: brown;
+}
+.continer>div:nth-child(126){
+    background-color: brown;
+}
+
+.hello{
+    display: grid;
+    grid-template-columns: repeat(18,1fr);
+    grid-template-rows: repeat(2,80px);
+    gap: 10px;
+    margin-top: 40px;
+}
+
+.hello>div:nth-child(1){
+    background-color: white;
+}.hello>div:nth-child(2){
+    background-color: white;
+}.hello>div:nth-child(3){
+    background-color: white;}
+    .hello>div:nth-child(4){
+    background-color:rgb(223, 12, 191) ;
+}.hello>div:nth-child(5){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(6){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(7){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(8){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(9){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(10){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(11){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(12){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(13){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(14){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(15){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(16){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(17){
+    background-color:rgb(223, 12, 191) ;
+}.hello
+>div:nth-child(18){
+    background-color:rgb(223, 12, 191) ;
+}.hello>div:nth-child(19){
+    background-color: white;
+}.hello>div:nth-child(20){
+    background-color: white;
+}.hello>div:nth-child(21){
+    background-color: white;}
+    .hello>div:nth-child(22){
+    background-color:rgb(211, 17, 17) ;
+}.hello>div:nth-child(23){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(24){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(25){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(26){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(27){
+    background-color:rgb(211, 17, 17);
+}.hello
+>div:nth-child(28){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(29){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(30){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(31){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(32){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(33){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(34){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(35){
+    background-color:rgb(211, 17, 17) ;
+}.hello
+>div:nth-child(36){
+    background-color:rgb(211, 17, 17);
+}
+#img{
+    flex-flow: wrap;
+    float: right ;
+    flex-flow: row;
+    float: right;
+}
+</style>
+
+<body>
+    <div class="continer">
+        <div><h1>H</h1></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div><h1>He</h1></div>
+        <div><h1>Li</h1></div>
+        <div><h1>Be</h1></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div><h1>B</h1></div>
+        <div><h1>C</h1></div>
+        <div><h1>N</h1></div>
+        <div><h1>O</h1></div>
+        <div><h1>F</h1></div>
+        <div><h1>Ne</h1></div>
+        <div><h1>Na</h1></div>
+        <div><h1>Mg</h1></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div><h1>Al</h1></div>
+        <div><h1>Si</h1></div>
+        <div><h1>P</h1></div>
+        <div><h1>s</h1></div>
+        <div><h1>Cl</h1></div>
+        <div><h1>Ar</h1></div>
+        <div><h1>K</h1></div>
+        <div><h1>Ca</h1></div>
+        <div><h1>Sc</h1></div>
+        <div><h1>Ti</h1></div>
+        <div><h1>V</h1></div>
+        <div><h1>cr</h1></div>
+        <div><h1>Mn</h1></div>
+        <div><h1>Fe</h1></div>
+        <div><h1>Co</h1></div>
+        <div><h1>Ni</h1></div>
+        <div><h1>Cu</h1></div>
+        <div><h1>Zn</h1></div>
+        <div><h1>Ga</h1></div>
+        <div><h1>Ge</h1></div>
+        <div><h1>As</h1></div>
+        <div><h1>Se</h1></div>
+        <div><h1>Br</h1></div>
+        <div><h1>Kr</h1></div>
+        <div><h1>Rb</h1></div>
+        <div><h1>Sr</h1></div>
+        <div><h1>Y</h1></div>
+        <div><h1>Zr</h1></div>
+        <div><h1>Nb</h1></div>
+        <div><h1>Mo</h1></div>
+        <div><h1>Te</h1></div>
+        <div><h1>Ru</h1></div>
+        <div><h1>Rh</h1></div>
+        <div><h1>Pd</h1></div>
+        <div><h1>Ag</h1></div>
+        <div><h1>Cd<h1></div>
+        <div><h1>In</h1></div>
+        <div><h1>Sn</h1></div>
+        <div><h1>Sb</h1></div>
+        <div><h1>Te</h1></div>
+        <div><h1>I</h1></div>
+        <div><h1>Xe</h1></div>
+        <div><h1>Cs</h1></div>
+        <div><h1>Ba</h1></div>
+        <div><img id="img" src="https://static.thenounproject.com/png/1729056-200.png" alt="" width="30px" style="margin-top: 25px;"><img id="img" src="https://static.thenounproject.com/png/1729056-200.png" alt="" width="30px"><img src="https://static.thenounproject.com/png/1729056-200.png" alt="" width="30px"><img src="https://static.thenounproject.com/png/1729056-200.png" alt="" width="20px"></div>
+        <div><h1>Hf</h1></div>
+        <div><h1>Ta</h1></div>
+        <div><h1>W</h1></div>
+        <div><h1>Re</h1></div>
+        <div><h1>Os</h1></div>
+        <div><h1>Ir</h1></div>
+        <div><h1>Pt</h1></div>
+        <div><h1>Au</h1></div>
+        <div><h1>Hg</h1></div>
+        <div><h1>Tl</h1></div>
+        <div><h1>Pd</h1></div>
+        <div><h1>Bi</h1></div>
+        <div><h1>Po</h1></div>
+        <div><h1>At</h1></div>
+        <div><h1>Rn</h1></div>
+        <div><h1>Fr</h1></div>
+        <div><h1>Ra</h1></div>
+        
+       <div>
+        <img id="img" src="/icons8-vertical-line-32.png" alt=""  ><img id="img" src="/icons8-vertical-line-32.png" alt=""><img src="https://cdn-icons-png.flaticon.com/512/109/109605.png" alt="" style="float: left;" width="30px">
+    </div>
+       <div><h1>Rf</h1></div>
+       <div><h1>Db</h1></div>
+       <div><h1>Sg</h1></div>
+       <div><h1>Bh</h1></div>
+       <div><h1>Hs</h1></div>
+       <div><h1>Mt</h1></div>
+       <div><h1>Ds</h1></div>
+       <div><h1>Rg</h1></div>
+       <div><h1>Cn</h1></div>
+       <div><h1>Uut</h1></div>
+       <div><h1>Fl</h1></div>
+       <div><h1>Uup</h1></div>
+       <div><h1>Lv</h1></div>
+       <div><h1>Uus</h1></div>
+       <div><h1>Uno</h1></div>
+       <!-- <div><h1></h1></div> -->
+        
+    </div>
+    <div class="hello">
+        <div></div>
+        <div></div>
+        <div>
+            <img src="/turn-right-arrow-with-broken-line.png" alt="" width="50px"  style="margin-left: 25px;" ><img src="/icons8-vertical-line-32.png" alt="" style="margin-left: -7px;"></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div><h1>Aa</h1></div>
+        <div></div>
+        <div></div>
+        <div><img src="/turn-right-arrow-with-broken-line.png" alt="" width="50px" style="margin-left: 5px; "></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+        <div><h1>Ve</h1></div>
+    </div>
+</body>
+</html>
